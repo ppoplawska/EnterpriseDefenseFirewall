079020a0eba9760e9a4be662cc21b99307b5bdb4
ppoplawska <patrickpoplawska@gmail.com>
2015-11-17 22:27:33 -0500

    Added Demo File, for testing purposes

--- firewall.c
+++ firewall.c
@@ -1,1 +1,17 @@
 // this is our Linux Firewall Project
+#define __KERNEL__
+#define MODULE
+#include <linux/ip.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/netdevice.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/skbuff.h>
+#include <linux/udp.h>
+static struct nf_hook_ops netfilter_ops;
+static unsigned char *ip_address = "\xC0\xA8\x00\x01"; 
+static char *interface = "lo";
+unsigned char *port = "\x00\x17";
+struct sk_buff *sock_buff;
+struct udphdr *udp_header;
