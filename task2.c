25d749130ff93ca5c32dce95394b8d77f6a7cd08
Seed <seed@ubuntu.(none)>
2015-11-28 09:53:19 -0800

    Progress on the next part

--- task2.c
+++ task2.c
@@ -9,6 +9,7 @@
 #include <linux/netfilter_ipv4.h>
 #include <linux/skbuff.h>
 #include <linux/udp.h>
+#include <linux/tcp.h>
 #include <linux/ip.h>
 
 static struct nf_hook_ops netfilter_ops;
@@ -17,8 +18,14 @@ static unsigned char *ip_address1 = "\x80\xE3\x09\x30"; //0x80E30930 ufl.edu
 static unsigned char *ip_address2 = "\x62\x8A\xFD\x6D"; //0x628AFD6D belongs to yahoo.com
 static char *interface = "lo";
 unsigned char *port = "\x00\x17";
+
+//FOR TCP STUFF
+unsigned int src_port;
+unsigned int dest_port;
+
 struct sk_buff *sock_buff;
 struct udphdr *udp_header;
+struct tcphdr *tcp_header;
 unsigned int main_hook(unsigned int hooknum,
                   struct sk_buff *skb,
                   const struct net_device *in,
@@ -30,6 +37,22 @@ unsigned int main_hook(unsigned int hooknum,
 
   //get ip header info
   struct iphdr *ip_header = (struct iphdr *)skb_network_header(skb);
+ 
+  //GET TCP HEADER
+  if (ip_header->protocol == 6) {
+	printk(KERN_INFO "Found TCP Packet: Protocol %d\n", ip_header->protocol);
+	tcp_header = (struct tcphdr *)((__u32 *)ip_header->ihl);
+	src_port = htons((unsigned short int)tcp_header->source);
+	dest_port = htons((unsigned short int)tcp_header->source);
+  }
+
+  //GET UDP HEADER
+  if (ip_header->protocol == 17) {
+	printk(KERN_INFO "Found UDP Packet: Protocl %d\n", ip_header->protocol);
+	udp_header = (struct udphdr *)((__u32 *)ip_header->ihl);
+	src_port = htons((unsigned short int)udp_header->source);
+	dest_port = htons((unsigned short int)udp_header->dest);
+  }
   //get source address
   unsigned int src_ip = (unsigned int)ip_header->saddr;
   if(!skb){
@@ -43,6 +66,12 @@ unsigned int main_hook(unsigned int hooknum,
   if(src_ip == *(unsigned int*)ip_address1){
 	printk(KERN_INFO "ufl ip is blocked!\n");
 	return NF_DROP;
+  }
+
+  printk(KERN_INFO "SRC: %d DEST: %d", src_port, dest_port);
+  if(dest_port == 23 || src_port == 23) {
+	printk(KERN_INFO "Telnet attempted: blocked.\n");
+  	return NF_DROP;
   }
   if(src_ip == *(unsigned int*)ip_address2){
 	printk(KERN_INFO "yahoo.com ip is blocked!\n");
