c06f750b16e2e3afbdf3f8c09828a42c94348ad9
root <root@ubuntu.(none)>
2015-12-01 19:59:13 -0800

    revamped code, now will drop incoming packets matching ip blacklist

--- task2.c
+++ task2.c
@@ -11,7 +11,7 @@
 #include <linux/udp.h>
 #include <linux/tcp.h>
 #include <linux/ip.h>
-
+//sd
 static struct nf_hook_ops netfilter_ops;
 //static unsigned char *ip_address = "\xC0\xA8\x00\x01";
 static unsigned char *ip_address1 = "\x80\xE3\x09\x30"; //0x80E30930 ufl.edu
@@ -26,64 +26,52 @@ unsigned int dest_port;
 struct sk_buff *sock_buff;
 struct udphdr *udp_header;
 struct tcphdr *tcp_header;
+
 unsigned int main_hook(unsigned int hooknum,
                   struct sk_buff *skb,
                   const struct net_device *in,
                   const struct net_device *out,
                   int (*okfn)(struct sk_buff*))
 {
+  
   printk(KERN_INFO "main hook\n");
-  if(strcmp(in->name,interface) == 0){ return NF_DROP; }
+  
+  sock_buff = skb;
 
   //get ip header info
-  struct iphdr *ip_header = (struct iphdr *)skb_network_header(skb);
+  struct iphdr *ip_header = (struct iphdr *)skb_network_header(sock_buff);
  
-  //GET TCP HEADER
-  if (ip_header->protocol == 6) {
-	printk(KERN_INFO "Found TCP Packet: Protocol %d\n", ip_header->protocol);
-	tcp_header = (struct tcphdr *)((__u32 *)ip_header->ihl);
-	src_port = htons((unsigned short int)tcp_header->source);
-	dest_port = htons((unsigned short int)tcp_header->source);
-  }
-
-  //GET UDP HEADER
-  if (ip_header->protocol == 17) {
-	printk(KERN_INFO "Found UDP Packet: Protocl %d\n", ip_header->protocol);
-	udp_header = (struct udphdr *)((__u32 *)ip_header->ihl);
-	src_port = htons((unsigned short int)udp_header->source);
-	dest_port = htons((unsigned short int)udp_header->dest);
-  }
+  char* UFL_EDU = "128.227.9.48";
+  char*	YAHOO_COM = "206.190.36.45";
+ 
   //get source address
   unsigned int src_ip = (unsigned int)ip_header->saddr;
-  if(!skb){
+  if(!sock_buff){
      printk(KERN_INFO "socket buffer is empty\n");
      return NF_ACCEPT;
   }
+  
   if(!ip_header){
 	printk(KERN_INFO "no ip header in the socket buffer!\n");
 	return NF_ACCEPT;
   }
-  if(src_ip == *(unsigned int*)ip_address1){
-	printk(KERN_INFO "ufl ip is blocked!\n");
-	return NF_DROP;
+  
+  // get ip and anaylise
+  
+  int ipSize = 15;
+  char * str[ipSize];
+  snprintf(str, ipSize, "%pI4", &ip_header->saddr);
+  
+  if(!strcmp(str, UFL_EDU) || !strcmp(str, YAHOO_COM)) // if we wnated to make this dynamic, register each of the banned IPS to an array
+														// figure out how to get user input for dynamic execution (piping mostlikey)
+  {
+		printk(KERN_INFO "Found blacklisted IP [ %s ] Dropping packet...\n", str);
+	  return NF_DROP; //drop it
   }
+  
 
-  printk(KERN_INFO "SRC: %d DEST: %d", src_port, dest_port);
-  if(dest_port == 23 || src_port == 23) {
-	printk(KERN_INFO "Telnet attempted: blocked.\n");
-  	return NF_DROP;
-  }
-  if(src_ip == *(unsigned int*)ip_address2){
-	printk(KERN_INFO "yahoo.com ip is blocked!\n");
-	return NF_DROP;
-  }
 
-/*
-if(sock_buff->nh.iph->protocol != 17){ return NF_ACCEPT; }
-udp_header = (struct udphdr *)(sock_buff->data + (sock_buff->nh.iph->ihl *4));
-if((udp_header->dest) == *(unsigned short*)port){ return NF_DROP; }
-*/
-
+printk(KERN_INFO "Did not hit any firewall rules, packet recieved...\n");
 return NF_ACCEPT;
 }
 int init_module()
