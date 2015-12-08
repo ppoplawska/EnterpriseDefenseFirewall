1bfa2dfef70ee3564b5e31af9075696963fe4214
etischler <etischler@ufl.edu>
2015-12-07 21:12:34 -0500

    Blocks JPGS on site and gives red stop sign

---
+++ blockjpgs.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+# Forces a flush after every write or print on the STDOUT
+select STDOUT; $| = 1;
+# Get the input line by line from the standard input.
+# Each line contains an URL and some other information.
+while (<>)
+{
+my @parts = split;
+my $url = $parts[0];
+# If you copy and paste this code from this PDF file,
+# the  ̃ (tilde) character may not be copied correctly.
+# Remove it, and then type the character manually.
+if ($url =~ /\.jpg/) {
+# URL Rewriting
+print "http://cdn2.ha-img.com/p/s4/117FTS029.jpg\n";
+}
+else {
+# No Rewriting.
+print "\n";
+}
+}
+
